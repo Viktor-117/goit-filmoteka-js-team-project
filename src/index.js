dd7ca7b3b167ee8da53777503f81e772937fc89a
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-09-11 00:19:42 +0300

    raw version markup for trending
    1. temporary constants in getTrending.js, because its some error in export
    2. instead of genres - ID, because there is a big piece of work with them

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import { toggleModal } from './film-modal';
-
 export const API_KEY = '520faa847257d57af54017c37ef43fe0';
+import { toggleModal } from './film-modal';
 import axios from 'axios';
 import { getTrending } from './js/getTrending.js';
 
@@ -10,5 +9,3 @@ const refs = {
   summitButton: document.querySelector('.submit-btn'),
   linkToTeam: document.querySelector('.footer__link'),
 };
-console.log(getTrending(4));
-console.log(getTrending(5));
--- src/js/getTrending.js
+++ src/js/getTrending.js
@@ -1,33 +1,56 @@
 import axios from 'axios';
+// import { API_KEY } from '../index.js';
 
-import { API_KEY } from '../index.js';
-import { refs } from '../index.js';
+// temporary constants, because its some error in export
+const refs = {
+  moviesList: document.querySelector('.film__list'),
+};
+const API_KEY = '520faa847257d57af54017c37ef43fe0';
+//end temp temporary constants
+
 let currentPage = 1;
+const srcImgBase = 'https://image.tmdb.org/t/p/w500';
 
+console.log(API_KEY);
 export async function getTrending(currentPage) {
   try {
-    return (responseTrending = await axios.get(
+    const resTrending = await axios.get(
       `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}&page=${currentPage}`
-    ));
+    );
+    return await resTrending.data;
   } catch (error) {
     console.log(error);
   }
 }
 
-// const renderTrendingList = movies => {
-//   const result = movies
-//     .map(({}) => {
-//       return `<li class="gallery__item">
-//             <img src="Film src" alt="film alt" class="img" />
-//             <div class="item__ptext">
-//               <h2 class="item__capt">Film Name</h2>
-//               <div class="item__wrap">
-//                 <p class="item__genre">Genres and date</p>
-//                 <p class="item__rating">rating</p>
-//               </div>
-//             </div>
-//           </li>`;
-//     })
-//     .join('');
-//   refs.moviesList.insertAdjacentHTML('beforeend', result);
-// };
+getTrending(currentPage).then(res => {
+  console.log(res.results.length);
+  if (res.results.length >= 1) {
+    const markup = res.results
+      .map(
+        ({
+          id,
+          title,
+          original_title,
+          poster_path,
+          genre_ids,
+          release_date,
+          vote_average,
+        }) => {
+          return `<li class="gallery__item">
+            <img src="${srcImgBase}${poster_path}" alt="${original_title}" class="img" />
+            <div class="item__ptext">
+              <h2 class="item__capt">${title}</h2>
+              <div class="item__wrap">
+                <p class="item__genre">${genre_ids} | ${release_date}</p>
+                <p class="item__rating">${vote_average}</p>
+              </div>
+            </div>
+          </li>`;
+        }
+      )
+      .join('');
+
+    refs.moviesList.insertAdjacentHTML('beforeend', markup);
+  }
+});
