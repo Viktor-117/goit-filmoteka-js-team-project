9ba4cea7873c796f11aa85c8694eda4193ecc07c
Oleksandr <Pidvysotskyi.oleksandr@gmail.com>
2022-09-10 11:53:17 +0300

    add refs and files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import axios from 'axios';
 import { getTrending } from './js/getTrending.js';
 const API_KEY = '520faa847257d57af54017c37ef43fe0';
+
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  searchInput: document.querySelector('.input'),
+  summitButton: document.querySelector('.submit-btn'),
+  linkToTeam: document.querySelector('.footer__link'),
+};
---
+++ src/library.js
@@ -0,0 +1,5 @@
+const refs = {
+  watchedBtn: document.querySelector('.watched_btn'),
+  queueBtn: document.querySelector('.queue_btn'),
+  linkToTeam: document.querySelector('.footer__link'),
+};
