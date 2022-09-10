054950002b8916c8c1fc2ed6b40e5b7f02a240a7
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-09-10 18:53:07 +0300

    Add function getTrending
    1. Adding function getTrending
    2. Changing index.js for correct export const API_KEY

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+export const API_KEY = '520faa847257d57af54017c37ef43fe0';
 import axios from 'axios';
 import { getTrending } from './js/getTrending.js';
-const API_KEY = '520faa847257d57af54017c37ef43fe0';
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
@@ -8,3 +8,5 @@ const refs = {
   summitButton: document.querySelector('.submit-btn'),
   linkToTeam: document.querySelector('.footer__link'),
 };
+console.log(getTrending(4));
+console.log(getTrending(5));
--- src/js/getTrending.js
+++ src/js/getTrending.js
@@ -1,3 +1,33 @@
 import axios from 'axios';
-const responseTrending = `https://api.themoviedb.org/3/trending/movie/day?api_key=520faa847257d57af54017c37ef43fe0`;
-console.log(responseTrending);
+
+import { API_KEY } from '../index.js';
+import { refs } from '../index.js';
+let currentPage = 1;
+
+export async function getTrending(currentPage) {
+  try {
+    return (responseTrending = await axios.get(
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}&page=${currentPage}`
+    ));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// const renderTrendingList = movies => {
+//   const result = movies
+//     .map(({}) => {
+//       return `<li class="gallery__item">
+//             <img src="Film src" alt="film alt" class="img" />
+//             <div class="item__ptext">
+//               <h2 class="item__capt">Film Name</h2>
+//               <div class="item__wrap">
+//                 <p class="item__genre">Genres and date</p>
+//                 <p class="item__rating">rating</p>
+//               </div>
+//             </div>
+//           </li>`;
+//     })
+//     .join('');
+//   refs.moviesList.insertAdjacentHTML('beforeend', result);
+// };
