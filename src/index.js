7931aeae5c03e22d3f6b208380e517c9d54f6b32
Oleksandr <Pidvysotskyi.oleksandr@gmail.com>
2022-09-11 14:11:39 +0300

    get genre by id

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export const API_KEY = '520faa847257d57af54017c37ef43fe0';
 import { toggleModal } from './film-modal';
 import axios from 'axios';
 import { getTrending } from './js/getTrending.js';
+import { getGenreById } from './js/getGenreById.js';
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
@@ -9,3 +10,5 @@ const refs = {
   summitButton: document.querySelector('.submit-btn'),
   linkToTeam: document.querySelector('.footer__link'),
 };
+
+getGenreById(12).then(genre => console.log(genre));
