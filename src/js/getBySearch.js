49884bbbcfce824d9202a216ae889b8979f4b0c5
Ihor Podvalniy <bembasua@gmail.com>
2022-09-12 13:49:48 +0300

    add pagination on search

--- src/js/getBySearch.js
+++ src/js/getBySearch.js
@@ -1,65 +1,91 @@
 import { API_KEY } from './key.js';
 
-
 /////////////////скрипт чомусь не працює, якщо імпортувати refs із index.js. Можливо, щось роблю неправильно
 // import { refs } from '../index.js';
 
 /////////////////продублював refs із index.js - так працює
 const refs = {
-    searchForm: document.querySelector('#search-form'),
-    searchInput: document.querySelector('.input'),
-    summitButton: document.querySelector('.submit-btn'),
-    linkToTeam: document.querySelector('.footer__link'),
-    moviesList: document.querySelector('.film__list'),
-  };
+  searchForm: document.querySelector('#search-form'),
+  searchInput: document.querySelector('.input'),
+  summitButton: document.querySelector('.submit-btn'),
+  linkToTeam: document.querySelector('.footer__link'),
+  moviesList: document.querySelector('.film__list'),
+};
 
-async function fetchMovies(inputQuery) {
-    const mainUrl = `https://api.themoviedb.org/3/search/movie`;
-    const filters = `?api_key=${API_KEY}&query=${inputQuery}`;
-    const response = await fetch(`${mainUrl}${filters}`);
-    return response.json();
-  };
+let currentPage = 1;
 
-let inputQuery = ""
-refs.searchForm.addEventListener("submit", onSearch);
+let totalPages = 1;
 
-export async function onSearch(event){
-    event.preventDefault()
-
-    inputQuery = refs.searchInput.value
-    console.log(inputQuery)
-    const newMovies = await fetchMovies(inputQuery)
-    const a = renderMoviesList(newMovies.results)
-    console.log(a)
-
+async function fetchMovies(inputQuery, currentPage) {
+  const mainUrl = `https://api.themoviedb.org/3/search/movie`;
+  const filters = `?api_key=${API_KEY}&query=${inputQuery}&page=${currentPage}`;
+  const response = await fetch(`${mainUrl}${filters}`);
+  return response.json();
 }
 
-function renderMoviesList(movies){
-    console.log(movies)
-    const markup = movies.map(
-      (
-        a
-        // id,
-        // title,
-        // original_title,
-        // poster_path,
-        // genre_ids,
-        // release_date,
-        // vote_average,
-      ) => {
-        return `<li class="gallery__item">
-          <img src="https://image.tmdb.org/t/p/w500${a.poster_path}" alt="${a.original_title}" class="img" />
-          <div class="item__ptext">
-            <h2 class="item__capt">${a.title}</h2>
-            <div class="item__wrap">
-              <p class="item__genre">${a.genre_ids} | ${a.release_date}</p>
-              <p class="item__rating">${a.vote_average}</p>
+let inputQuery = '';
+refs.searchForm.addEventListener('submit', onSearch);
+
+export async function onSearch(event) {
+  event.preventDefault();
+
+  inputQuery = refs.searchInput.value;
+  console.log(inputQuery);
+  addPagination();
+}
+
+export default async function renderMoviesList(pageNumber) {
+  currentPage = pageNumber;
+
+  await fetchMovies(inputQuery, currentPage).then(res => {
+    if (res.results.length >= 1) {
+      markup = res.results.map(
+        ({
+          id,
+          title,
+          original_title,
+          poster_path,
+          genre_ids,
+          release_date,
+          vote_average,
+        }) => {
+          return `<li class="gallery__item">
+            <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${original_title}" class="img" />
+            <div class="item__ptext">
+              <h2 class="item__capt">${title}</h2>
+              <div class="item__wrap">
+                <p class="item__genre">${genre_ids} | ${release_date}</p>
+                <p class="item__rating">${vote_average}</p>
+              </div>
             </div>
-          </div>
-        </li>`;
+          </li>`;
+        }
+      );
+
+      return markup;
+    }
+  });
+}
+
+async function addPagination() {
+  await fetchMovies(inputQuery, currentPage).then(res => {
+    totalPages = res.total_pages;
+    return totalPages;
+  });
+  $(`#pagination-container`).pagination({
+    dataSource: function (done) {
+      var result = [];
+      for (var i = 1; i < totalPages; i++) {
+        result.push(i);
       }
-    )
-    .join('');
-  refs.moviesList.innerHTML = markup;
- 
+      done(result);
+    },
+    pageSize: 20,
+    callback: async function (data, pagination) {
+      await renderMoviesList(pagination.pageNumber);
+      // template method of yourself
+      var html = markup;
+      $(`.film__list`).html(html);
+    },
+  });
 }
