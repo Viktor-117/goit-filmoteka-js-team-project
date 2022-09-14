1492c84d2c2bca879308e100b732d71254c095b5
Viktor-117 <viktor.hushchin@gmail.com>
2022-09-14 19:12:36 +0300

    update film modal

--- src/js/film-modal.js
+++ src/js/film-modal.js
@@ -3,25 +3,45 @@ import filmCardTemplate from '../hbs/modal-film-card.hbs';
 const refs = {
   openFilmModal: document.querySelector('[data-modal-open]'),
   closeFilmModal: document.querySelector('[data-modal-close]'),
+  closeFilmModalBtn: document.querySelector('[data-modal-close-btn]'),
   filmModal: document.querySelector('[data-film-modal]'),
   filmCard: document.querySelector('[data-film-card]'),
   modalFilm: document.querySelector('.modal-film'),
 };
 
 refs.openFilmModal.addEventListener('click', onCardClick);
-refs.closeFilmModal.addEventListener('click', toggleModal);
+// refs.closeFilmModal.addEventListener('click', toggleModal);
+
+export function openFilmModal() {
+  refs.filmModal.classList.remove('is-hidden');
+  refs.closeFilmModal.addEventListener('click', closeFilmModal);
+  refs.closeFilmModalBtn.addEventListener('click', closeFilmModal);
+  window.addEventListener('keydown', closeFilmModal);
+}
 
 export function closeFilmModal(e) {
-  if (e.key === 'Escape') {
-    toggleModal();
+  if (
+    e.target === refs.closeFilmModal ||
+    e.currentTarget === refs.closeFilmModalBtn
+  ) {
+    refs.filmModal.classList.add('is-hidden');
+    return;
+  } else if (e.key === 'Escape') {
+    refs.filmModal.classList.add('is-hidden');
     window.removeEventListener('keydown', closeFilmModal);
   }
 }
 
-export function toggleModal() {
-  refs.filmModal.classList.toggle('is-hidden');
-  window.addEventListener('keydown', closeFilmModal);
-}
+// if (e.key === 'Escape') {
+//   toggleModal();
+//   window.removeEventListener('keydown', closeFilmModal);
+//   }
+// }
+
+// export function toggleModal() {
+//   refs.filmModal.classList.toggle('is-hidden');
+//   window.addEventListener('keydown', closeFilmModal);
+// }
 function renderFilmInfo(filmData) {
   const markup = filmCardTemplate(filmData);
   refs.filmCard.innerHTML = markup;
@@ -31,7 +51,8 @@ function onCardClick(event) {
   const filmId = event.target.getAttribute('id');
   filmId && showFilmInfo(filmId);
   refs.modalFilm.id = filmId;
+  openFilmModal();
 }
 export function showFilmInfo(movieId) {
-  getById(movieId).then(renderFilmInfo).then(toggleModal).catch(console.log);
+  getById(movieId).then(renderFilmInfo).then(openFilmModal).catch(console.log);
 }
